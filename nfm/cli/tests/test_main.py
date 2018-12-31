78bd770a7e5d7b9444b7d189135e257c6eb112e8
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-12-30 22:33:21 -0500

    TEST: Test unimplemented commands, utility options

---
+++ nfm/cli/tests/test_main.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+import pytest
+from click.testing import CliRunner
+from .. import main
+
+
+@pytest.mark.parametrize("option", ["--help", "--version"])
+def test_utility_options(option):
+    runner = CliRunner()
+    result = runner.invoke(main, [option])
+    assert result.exit_code == 0
+
+
+@pytest.mark.parametrize("command", ["test", "install"])
+def test_unimplemented(command):
+    runner = CliRunner()
+    result = runner.invoke(main, [command])
+    assert result.exit_code == 0
