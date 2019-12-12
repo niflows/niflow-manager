642e158e1cb53063202edebc4c1a10cd6592e980
Dorota Jarecka <djarecka@gmail.com>
2019-12-12 00:57:04 -0500

    adding nfm tests command

--- niflow_manager/cli/main.py
+++ niflow_manager/cli/main.py
@@ -1,4 +1,5 @@
 import click
+import subprocess as sp
 from .. import __version__
 from .init import init
 
@@ -18,8 +19,19 @@ def install():
 
 
 @main.command()
-def test():
-    print("test command not yet implemented")
+@click.argument('workflow_path', type=click.Path(), default='.')
+@click.option('-w', '--working-dir', type=click.Path(),
+              help="Working directory, default is a temporary directory.")
+
+def test(workflow_path, working_dir=None):
+    #path, full_name, organization, workflow = normalize_path(name)
+    print(f'testing {workflow_path}')
+    #path.mkdir(parents=True, exist_ok=True)
+    if working_dir:
+        sp.run(['testkraken', str(workflow_path), "-w", working_dir], check=True)
+    else:
+        sp.run(['testkraken', str(workflow_path)], check=True)
+
 
 
 @main.command()
