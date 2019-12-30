59555547e2c3300d6516f4fb9a339c7933dfa729
Dorota Jarecka <djarecka@gmail.com>
2019-12-29 23:21:53 -0500

    adding test for building a python niflow

--- niflow_manager/cli/tests/test_main.py
+++ niflow_manager/cli/tests/test_main.py
@@ -1,5 +1,6 @@
 import pytest
 from click.testing import CliRunner
+from pathlib import Path
 from .. import main
 
 
@@ -15,3 +16,26 @@ def test_commads_help(command):
     runner = CliRunner()
     result = runner.invoke(main, [command, "--help"])
     assert result.exit_code == 0
+
+
+def test_init_build_python():
+    """ initilize a niflow for python and build a container using the default spec"""
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        res_init = runner.invoke(
+            main, ["init", "--language", "python", "wf"], "\n".join(["org", "wf", "y"])
+        )
+        niflow_path = Path.cwd() / "niflow-org-wf"
+        spec_path = niflow_path / "spec.yml"
+        assert res_init.exit_code == 0
+        assert spec_path.exists()
+
+        # uncommenting the spec file
+        with spec_path.open() as f:
+            spec_orig = f.readlines()
+        with spec_path.open(mode="w") as f:
+            for line in spec_orig:
+                f.write(line[1:])
+
+        res_build = runner.invoke(main, ["build", str(niflow_path)])
+        assert res_build.exit_code == 0
